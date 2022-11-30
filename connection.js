8efda5f63c99dc4daeaf259507df3c79bcb23340
feri <ferialfahri@gmail.com>
2022-11-30 11:46:19 +0000

    Adding production env

--- connection.js
+++ connection.js
@@ -1,8 +1,17 @@
 const knex = require("knex");
 const dotenv = require("dotenv");
 
+const ENV = process.env.NODE_ENV || "development";
+
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error("PGDATABASE or DATABASE_URL not set");
+}
+
+const config =
+  ENV === "production" ? { connectionString: process.env.DATABASE_URL } : {};
+
 dotenv.config();
-const {PGUSER, PGPASSWORD, PGDATABASE} = process.env;
+const { PGUSER, PGPASSWORD, PGDATABASE } = process.env;
 
 exports.db = knex({
   client: "pg",
