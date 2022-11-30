6a1448818ec8941d3996113b0f6b6047643d37f0
feri <ferialfahri@gmail.com>
2022-11-30 12:09:21 +0000

    Debugging

--- connection.js
+++ connection.js
@@ -7,16 +7,13 @@ if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error("PGDATABASE or DATABASE_URL not set");
 }
 
-const config =
-  ENV === "production" ? { connectionString: process.env.DATABASE_URL } : {};
-
 dotenv.config();
-const { PGUSER, PGPASSWORD, PGDATABASE } = process.env;
+const { PGUSER, PGPASSWORD, PGDATABASE, DATABASE_URL } = process.env;
 
 exports.db = knex({
   client: "pg",
   connection: {
-    host: "127.0.0.1",
+    host: DATABASE_URL,
     user: PGUSER,
     password: PGPASSWORD,
     database: PGDATABASE,
